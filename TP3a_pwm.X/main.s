c548232c778f270cb81d96fb0b3a939f97c225ff
Sensitor <nicolas.gouaux@gmail.com>
2025-12-11 15:31:19 +0100

    TP3

---
+++ TP3a_pwm.X/main.s
@@ -0,0 +1,75 @@
+PROCESSOR 18F25K40
+#include <xc.inc>
+
+;===============================================================================
+; CONFIGURATION
+;===============================================================================
+config FEXTOSC = OFF
+config RSTOSC  = HFINTOSC_64MHZ
+config WDTE    = OFF
+
+PSECT code, abs
+
+org 0x000
+    goto init
+
+org 0x008
+    goto High_ISR
+
+org 0x018
+    goto Low_ISR
+
+
+org 0x100
+
+init:
+
+    movlw 0x07            ; PWM3 function
+    BANKSEL RC0PPS
+    movwf RC0PPS
+
+    BANKSEL TRISC
+    bcf TRISC,0          ; On clear la led0 pour l'enable
+
+    BANKSEL PWM3DCH
+    movlw 0b00110010
+    movwf PWM3DCH
+
+; Enable PWM3
+    BANKSEL PWM3CON
+    movlw 0b10000000
+    movwf PWM3CON
+
+
+; TIMER2 en 125 Hz
+
+    BANKSEL T2CON
+    movlw 0b11011111  ; prescaler 1:32, postscaler 1:16, 
+    movwf T2CON
+
+    BANKSEL T2CLKCON
+    movlw 0x01          ; Fosc/4
+    movwf T2CLKCON
+
+    BANKSEL T2PR
+    movlw 0b11111001 ; PR2 à 249
+    movwf T2PR	     ;
+   
+    
+    goto loop
+
+    
+    
+; Boucle infinie
+;------------------------------------------------------------
+loop:
+    goto loop
+
+; Routines d'interruption ======================================================   
+High_ISR:
+    retfie
+
+Low_ISR:
+    retfie
+
+end
